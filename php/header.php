e2aad4632b27c608c398bbb44f94571e3b8567b2
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-09 10:40:43 +0100

    feat: php/header.php. Criação de novo arquivo

---
+++ php/header.php
@@ -0,0 +1,7 @@
+<?php
+
+    // $conexao = mysqli_connect("localhost", "root", "", "sdq");
+
+    $conexao= mysqli_connect("sql302.infinityfree.com", "if0_34723075", "Ca7CTk1ObV8a", "if0_34723075_sdq");
+
+?>
